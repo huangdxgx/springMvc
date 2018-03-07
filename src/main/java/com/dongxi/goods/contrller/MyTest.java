f887be87c798e2d68b8c81c031da97ce505d2b44
huangdx <huangdongxi126@126.com>
2018-03-07 21:36:12 +0800

    你好

---
+++ src/main/java/com/dongxi/goods/contrller/MyTest.java
@@ -0,0 +1,11 @@
+package com.dongxi.goods.contrller;
+
+/**
+ * @Author: huangdx_gx
+ * @Decscription: what is the class useing for
+ * @date: 2018/3/7 - 21:35
+ * @see: com.dongxi.goods.contrller
+ */
+public class MyTest {
+}
+
