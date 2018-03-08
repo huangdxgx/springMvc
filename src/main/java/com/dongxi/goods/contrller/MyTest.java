12d913c1df0cd1a270ba6ed884bc409b72a1b8f0
huangdx <huangdongxi126@126.com>
2018-03-08 11:07:54 +0800

    第二次修改

--- src/main/java/com/dongxi/goods/contrller/MyTest.java
+++ src/main/java/com/dongxi/goods/contrller/MyTest.java
@@ -1,5 +1,7 @@
 package com.dongxi.goods.contrller;
 
+import com.dongxi.goods.entity.User;
+
 /**
  * @Author: huangdx_gx
  * @Decscription: what is the class useing for
@@ -9,6 +11,9 @@ package com.dongxi.goods.contrller;
 public class MyTest {
     public  static  void  main(String[] args){
         System.out.println("这是我的一个小测试！");
+        User user = new User();
+        user.setUserId(1);
+        user.setUserAddress("南宁");
     }
 }
 
